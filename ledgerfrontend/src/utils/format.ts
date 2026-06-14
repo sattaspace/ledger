3f7248293946d49be0b20426edc611ac85d9dc7e
haradhansharma <haradhan.sharma@gmail.com>
2026-06-14 12:10:01 +0600

    reduce

--- ledgerfrontend/src/utils/format.ts
+++
@@ -1,6 +0,0 @@
-export const formatCurrency = (amount: number): string => {
-  return new Intl.NumberFormat('en-US', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }).format(amount);
-};
